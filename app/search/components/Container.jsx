f99f01df5fa405dbcbb1902a90c5575dc69b3fbe
Ameer77-tech <ameershaik.cs@gmail.com>
2025-10-10 20:29:20 +0530

    stable

--- app/search/components/Container.jsx
+++ app/search/components/Container.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Container = ({ results }) => {
   return (
     <div className="text-background-foreground relative w-full px-5 place-items-center md:gap-8 gap-7 md:px-55 grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 md:py-10 py-5 mt-10">
-      {results?.length < 1 ? (
+      {results?.length < 1 || results == undefined ? (
         <p className="absolute text-muted-foreground">NOT FOUND</p>
       ) : (
         results
