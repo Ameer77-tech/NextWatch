ad6fa3b4ee336b816c1825394d84ff91372c0649
Ameer77-tech <ameershaik.cs@gmail.com>
2025-10-23 19:40:17 +0530

    Edited next config for !image broken to appear

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://next-watch-coral.vercel.app/",
+      },
+    ],
   },
 };
 process.on("uncaughtException", (err) => {
